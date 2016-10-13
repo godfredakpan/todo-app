2db00207a58168794d3e295ad89e43df50f7bd71
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-10-13 21:24:55 +0100

    Filter todos and display on template

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from . import views
+from .views import HomeView
 
 
 app_name = 'todoapp'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.views import View
+
+from .models import Label, ToDoList
 
 
-def index(request):
-    return render(request, 'todoapp/home.html', {})
+class HomeView(View):
+
+    def get(self, request):
+        labels = Label.objects.all()
+        pending_todos = ToDoList.objects.filter(status='P')
+        missed_todos = ToDoList.objects.filter(status='M')
+        completed_todos = ToDoList.objects.filter(status='C')
+
+        context = {
+            'labels': labels,
+            'pending_todos': pending_todos,
+            'missed_todos': missed_todos,
+            'completed_todos': completed_todos
+        }
+        return render(request, 'todoapp/home.html', context)
